0cc6b309106d472f813ed1da9d00ef92814bf820
Neelima Patnaik <51104846+neelup84@users.noreply.github.com>
2020-09-17 21:56:45 -0500

    Add files via upload

---
+++ My First Java Project/src/com/neelima/MyClass.java
@@ -0,0 +1,16 @@
+package com.neelima;
+
+public class MyClass {
+	
+	int myInt = 5;
+	
+	MyClass(){
+		System.out.println("constructor");
+	}
+	
+	void myMethod() {
+		
+		System.out.println("My Method");
+	}
+
+}
---
+++ My First Java Project/src/com/neelima/Review.java
@@ -0,0 +1,15 @@
+package com.neelima;
+
+public class Review {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyClass mc = new MyClass();
+		mc.myMethod();
+		mc.myInt = 10;
+		
+		System.out.println(mc.myInt);
+
+	}
+
+}
